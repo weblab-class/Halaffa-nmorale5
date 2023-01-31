1cd208a2d5b43db5b256b0cb425a9209e29fe647
Adam Bundy <halaffamagician@gmail.com>
2023-01-31 17:42:25 -0500

    minor things

--- client/src/attributes/moves.js
+++ client/src/attributes/moves.js
@@ -1,24 +1,66 @@
-function damage(player, enemy, amount) {
-  enemy.health -= amount
-}
+const equipment = require('../client/src/attributes/equipment.json');
+const equipmentFuncs = require('../client/src/attributes/equipment.js');
 
-const moves = {
-  0: (player, enemy) => {
-    damage(player, enemy, 1);
+const getEquipment = (eqId) => {
+  const eq = { ...equipment.find(({id}) => id === eqId) };
+  eq.func = equipmentFuncs[eqId];
+  return eq;
+};
+
+/**
+ * move: {
+ *  power
+ *  accuracy
+ *  color
+ * }
+ * 
+ * player/enemy: {
+ *  attack
+ *  health
+ *  speed
+ *  equipment
+ *  moves
+ * }
+ */
+
+// Note: moves should never tamper with other moves, player stats, or equipment 
+// (unless it's adding a new equipment).
+
+const moveFuncs = {
+  0: (move, player, enemy) => {
+    // deal some damage
+    return { player: 0, enemy: -move.power * .5 };
   },
-  1: (player, enemy) => {
-    damage(player, enemy, 2);
+  100: (move, player, enemy) => {
+    // deal a some damage and heal a bit
+    return { player: move.power * .2, enemy: -move.power * .5 };
   },
-  2: (player, enemy) => {
-    damage(player, enemy, 3);
+  101: (move, player, enemy) => {
+    // cast storm on both players
+    player.equipment.push(getEquipment(217));
+    enemy.equipment.push(getEquipment(217));
+    return { player: 0, enemy: 0 };
+  },
+  102: (move, player, enemy) => {
+    // damage enemy for each of their red moves, and heal for each of your green moves
+    let redMult = 0;
+    let greenMult = 0;
+    enemy.moves.forEach(move => {
+      if (move.color == 'red') redMult++;
+    });
+    player.moves.forEach(move => {
+      if (move.color == 'green') greenMult++;
+    })
+    return { player: move.power * greenMult, enemy: -move.power * redMult };
+  },
+};
+
+module.exports = (move, player, enemy) => {
+  if (move.accuracy > Math.random()) {
+    const healthDiffs = moveFuncs[move.id](move, player, enemy);
+    player.health += healthDiffs.player;
+    enemy.health += healthDiffs.enemy;
+    return { ...healthDiffs, missed: false };
   }
+  return { player: 0, enemy: 0, missed: true }
 }
-
-function resolveMove(playerStats, enemyStats, moveId) {
-  let player = {...playerStats};
-  let enemy = {...enemyStats};
-  moves[moveId](player, enemy);
-  return [player, enemy];
-}
-
-module.exports = resolveMove;
